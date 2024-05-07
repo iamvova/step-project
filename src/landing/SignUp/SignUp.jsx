3ab516685986c67da89327c875c076e40750b25a
iamvova <vvolodya1110@gmail.com>
2024-05-07 20:17:35 +0300

    add request to back

--- src/landing/SignUp/SignUp.jsx
+++ src/landing/SignUp/SignUp.jsx
@@ -1,24 +1,119 @@
 import React, { useEffect, useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { Link } from 'react-router-dom'
+import { useNavigate  } from 'react-router-dom';
 import { CgClose } from 'react-icons/cg'
 import '../container/Menu/Menu.scss'
 import './SignUp.scss'
 
+import Web3 from 'web3'
+
 const SignUp = () => {
+    const [address, setAddress] = useState()
     const [toggleMenu, setToggleMenu] = useState(false)
     const [hideBreger, setHideBreger] = useState(true)
+    const [web3, setWeb3] = useState(null)
+    const [accounts, setAccounts] = useState([])
+    const [buttonClicked, setButtonClicked] = useState(false)
+    let navigate = useNavigate(null);
+    
     useEffect(()=>{
         if(toggleMenu === true){
-            document.querySelector('body').style.overflowY = 'hidden';
+            document.querySelector('body').style.overflowY = 'hidden'
         }else {
-            document.querySelector('body').style.overflowY = 'auto';
+            document.querySelector('body').style.overflowY = 'auto'
         }
     })
 
     const handleOpenMenu = ()=>{
         setToggleMenu(current => !current)
         setHideBreger(current => !current)
+    }
+
+    const connectMetamask = async () => {
+      if (window.ethereum) {
+        const web3Instance = new Web3(window.ethereum)
+        try {
+          // Запит на отримання дозволу на доступ до рахунків MetaMask
+          await window.ethereum.enable()
+          // Отримання списку рахунків MetaMask
+          const accs = await web3Instance.eth.getAccounts()
+          // Відправка зв'язаного рахунку на бекенд
+          if (accs.length > 0) {
+            setAddress(accs[0])
+            sendAccountToBackend(accs[0])
+          }
+        } catch (error) {
+          console.error('Access denied to MetaMask accounts:', error)
+        }
+      } else {
+        console.error('Please install MetaMask extension')
+      }
+    };
+    
+    const sendAccountToBackend = async (account) => {
+      const backendURL = 'http://195.189.226.95/user/registration/'
+    
+      try {
+        setAddress(account);
+        const response = await fetch(`${backendURL}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ public_key: account }),
+        });
+    
+        if (response.ok) {
+          const data = await response.json()
+          // Отримати капчу з відповіді сервера
+          const captcha = data.captcha
+          // Підписати капчу MetaMask
+          signMessageWithMetaMask(captcha, account)
+        } else {
+          console.error('Failed to send MetaMask account to backend')
+        }
+      } catch (error) {
+        console.error('Error sending MetaMask account to backend:', error)
+      }
+    };
+    
+    const signMessageWithMetaMask = async (captcha, account) => {
+      if (!window.ethereum) {
+        console.error('MetaMask not available')
+        return
+      }
+    
+      try {
+        const web3Instance = new Web3(window.ethereum)
+        const signature = await web3Instance.eth.personal.sign(captcha, account, '')
+        console.log('Signature:', signature)
+        sendSignatureToBackend(account, signature)
+      } catch (error) {
+        console.error('Error signing message with MetaMask:', error)
+      }
+    }
+
+    const sendSignatureToBackend = async (account, signature) => {
+      const backendURL = 'http://195.189.226.95/user/login/'
+      try {
+        console.log("121212121", account)
+        const response = await fetch(`${backendURL}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ signature: signature, public_key: account }),
+        })
+    
+        if (response.ok) {
+          const data = await response.json()
+          console.log('Access Token:', data)
+        } else {
+          console.error('Failed to send signature to backend')
+        }
+      } catch (error) {
+        console.error('Error sending signature to backend:', error)
+      }
     }
 
   return (
@@ -57,7 +152,17 @@ const SignUp = () => {
                 <div className="container__meta-img">
                     <img src="https://kit.baliniz.com/cryptoxo/wp-content/uploads/sites/9/2021/05/secure.png" alt="1" />
                 </div>
-                <Link to="/profile"> <button className='connect__meta def__btn'>Connect Metamask</button></Link>
+                <button className='connect__meta def__btn' onClick={connectMetamask}>Connect Metamask</button>
+                {buttonClicked && (
+                    <div>
+                        <h2>Connected accounts:</h2>
+                        <ul>
+                            {accounts.map((account, index) => (
+                            <li key={index}>{account}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
 
